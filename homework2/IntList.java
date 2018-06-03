bfb7ed3539e66b72f88f7ba375f3640c65761972
Vorting <39590607+Vorting@users.noreply.github.com>
2018-06-03 22:34:41 +0300

    Add files via upload
    /*Создать интерфейс IntList с методами: add(int element),
    add (int index, int element), clear(), contains  int value),
    get(int index), isEmpty(), remove(int index), removeElement(int element),
    set(int index, int element), size(). И 2 его реализации
    - IntArrayList и IntLinkedList по аналогии с
    ArrayList<Integer> и LinkedList<Integer>.
     */

---
+++ homework2/IntList.java
@@ -0,0 +1,12 @@
+package com.homework2;
+
+public interface IntList {
+    void add(int value);
+
+    void add(int value, int index);
+
+    int get(int index) throws IllegalAccessException;
+
+    int size();
+
+}
