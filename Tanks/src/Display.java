c88ab94098a7dadd610e070c967c2a7714de163c
dima ivanov <ya.warkus@gmail.com>
2019-04-25 10:19:14 +0300

    create new Tanks

---
+++ Tanks/src/Display.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+import java.awt.*;
+
+public abstract class Display {
+
+    private static boolean created = false;
+    private static JFrame window;
+    private static Canvas content;
+
+    public static void create(int width, int height, String title) {
+        if (created) {
+            return;
+        }
+        window = new JFrame(title);
+        content = new Canvas();
+
+        Dimension size = new Dimension(width, height);
+        content.setPreferredSize(size);
+
+        window.setResizable(false);
+        window.getContentPane().add(content);
+        window.pack();
+        window.setVisible(true);
+    }
+}
