2d20f651c97344cbb74234386948ba6099b0dbac
Dima Ivanov <ya.warkus@gmail.com>
2019-07-10 11:30:57 +0300

    add missing exercise GalToLitTableDemo

---
+++ shildt/src/com/edition5/chapter2/Theory/GalToLitTableDemo.java
@@ -0,0 +1,22 @@
+package com.edition5.chapter2.Theory;
+
+public class GalToLitTableDemo {
+    public static void main(String[] args) {
+        double gallons, liters;
+        int counter;
+
+        counter = 0; // устанавлвиваем нулевое значение
+        for (gallons = 1; gallons <= 100; gallons++) {
+            liters = gallons * 3.7854; // преобразуем в литр
+            System.out.println(gallons + " gallons is " + liters + " liters.");
+
+            // приращение счетчика строк происходит на каждом шаге цикла,
+            // а через каждые десять строк выводится пустая строка
+            counter++;
+            if (counter == 10) {
+                System.out.println();
+                counter = 0; //сбрасываем счетчик
+            }
+        }
+    }
+}
